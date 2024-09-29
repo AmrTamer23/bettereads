b9a83dce47e883afdfbea13caa319f7958183564
AmrTamer23 <amrtamer23mj@gmail.com>
2024-09-29 15:26:25 +0300

    feat: implemented admin route with approve and reject book handlers and a middleware to block normal user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,20 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import protectedRoutes from "./protected-routes";
+import userRoutes from "./user-routes";
 import publicRoutes from "./public-routes";
+import adminRoutes from "./admin-routes";
 import { protect } from "../middleware/auth";
+import { protectAdmin } from "../middleware/authorize";
 
 export const app = new Hono();
 
 app.route("/auth", publicRoutes);
 
 app.use("/*", protect);
-app.route("/", protectedRoutes);
+app.route("/", userRoutes);
+
+app.use("/admin/*", protectAdmin);
+app.route("/admin/", adminRoutes);
 
 const port = Number(process.env.PORT) || 3005;
 console.log(`Server is running on port ${port}`);
