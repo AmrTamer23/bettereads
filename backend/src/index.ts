5122ea5300b18d9dff5d1769da18a53f1099f2bf
AmrTamer23 <amrtamer23mj@gmail.com>
2024-10-11 12:22:17 +0300

    chore: add cors middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,8 +5,16 @@ import publicRoutes from "./public-routes";
 import adminRoutes from "./admin-routes";
 import { protect } from "../middleware/auth";
 import { protectAdmin } from "../middleware/authorize";
+import { cors } from "hono/cors";
 
 export const app = new Hono();
+
+app.use(
+  "/*",
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.route("/auth", publicRoutes);
 
